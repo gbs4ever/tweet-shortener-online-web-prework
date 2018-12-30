44c88e2d7d9423aa4a7bcfd2b3b457ed02c4163b
Sim Greenbaum <gbs4ever2@gmail.com>
2018-12-30 05:10:18 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -38,7 +38,8 @@ end
 
 def selective_tweet_shortener(strings)
   if strings.length  >  140
-    puts word_substituter(strings)
-else puts string
+     word_substituter(strings)
+else
+  puts string
 end
 end
